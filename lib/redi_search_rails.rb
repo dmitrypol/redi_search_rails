aa004aea521b1a7ce2bebb40a8b54c7250a68dc6
Dmitry Polyakovsky <dmitrypol@gmail.com>
2017-04-22 22:01:22 -0700

    fixed variable scope issues bump gem version added tests changed gem dependency requirements

--- lib/redi_search_rails.rb
+++ lib/redi_search_rails.rb
@@ -14,11 +14,11 @@ module RediSearchRails
     # @see https://github.com/dmitrypol/redi_search_rails
     # @param schema [Hash]  name: 'TEXT', age: 'NUMERIC'
     def redi_search_schema(schema)
-      @@schema = schema.to_a.flatten
-      @@fields = schema.keys
-      @@model = self.name.constantize
-      @@index_name = @@model.to_s
-      @@score = 1
+      @schema = schema.to_a.flatten
+      @fields = schema.keys
+      @model = self.name.constantize
+      @index_name = @model.to_s
+      @score = 1
     end
 
     # search the index for specific keyword(s)
@@ -27,7 +27,7 @@ module RediSearchRails
     # @return [Array]   [{"id": "gid://application_name/User/unique_id", "name": "Bob", "age": "100"}, ...]
     # @raise [RuntimeError]
     def ft_search keyword
-      results = REDI_SEARCH.call('FT.SEARCH', @@index_name, keyword,
+      results = REDI_SEARCH.call('FT.SEARCH', @index_name, keyword,
        #'LIMIT', 0, 1000,
        #'NOCONTENT', #'VERBATIM',  #'WITHSCORES', #'NOSTOPWORDS', #'WITHPAYLOADS',
       )
@@ -52,9 +52,9 @@ module RediSearchRails
     #
     # @return [String]
     def ft_create
-      REDI_SEARCH.call('FT.CREATE', @@index_name,
+      REDI_SEARCH.call('FT.CREATE', @index_name,
         #'NOFIELDS', 'NOSCOREIDX', 'NOOFFSETS',
-        'SCHEMA', @@schema
+        'SCHEMA', @schema
       )
       ft_optimize
     rescue Exception => e
@@ -66,7 +66,7 @@ module RediSearchRails
     #
     # @return [String]
     def ft_add_all
-      @@model.all.each {|record| ft_add(record) }
+      @model.all.each {|record| ft_add(record) }
     rescue Exception => e
       Rails.logger.error e if defined? Rails
       return e.message
@@ -78,8 +78,8 @@ module RediSearchRails
     # @return [String]
     def ft_add record
       fields = []
-      @@fields.each { |field| fields.push(field, record.send(field)) }
-      REDI_SEARCH.call('FT.ADD', @@index_name, record.to_global_id.to_s, @@score,
+      @fields.each { |field| fields.push(field, record.send(field)) }
+      REDI_SEARCH.call('FT.ADD', @index_name, record.to_global_id.to_s, @score,
         'REPLACE',
         #'NOSAVE', 'PAYLOAD', record.name,
         'FIELDS', fields
@@ -95,7 +95,7 @@ module RediSearchRails
     # @return [String]
     def ft_del record
       doc_id = record.to_global_id
-      REDI_SEARCH.call('FT.DEL', @@index_name, doc_id)
+      REDI_SEARCH.call('FT.DEL', @index_name, doc_id)
     rescue Exception => e
       Rails.logger.error e if defined? Rails
       return e.message
@@ -105,7 +105,7 @@ module RediSearchRails
     #
     # @return [String]
     def ft_optimize
-      REDI_SEARCH.call('FT.OPTIMIZE', @@index_name)
+      REDI_SEARCH.call('FT.OPTIMIZE', @index_name)
     rescue Exception => e
       Rails.logger.error e if defined? Rails
       return e.message
@@ -115,7 +115,7 @@ module RediSearchRails
     #
     # @return [String]
     def ft_drop
-      REDI_SEARCH.call('FT.DROP', @@index_name)
+      REDI_SEARCH.call('FT.DROP', @index_name)
     rescue Exception => e
       Rails.logger.error e if defined? Rails
       return e.message
@@ -125,7 +125,7 @@ module RediSearchRails
     #
     # @return [String]
     def ft_info
-      REDI_SEARCH.call('FT.INFO', @@index_name)
+      REDI_SEARCH.call('FT.INFO', @index_name)
     rescue Exception => e
       Rails.logger.error e if defined? Rails
       return e.message
--- spec/redi_search_rails_spec.rb
+++ spec/redi_search_rails_spec.rb
@@ -16,26 +16,32 @@ RSpec.describe RediSearchRails do
   it "redi_search_schema" do
     test = User.redi_search_schema(name: 'TEXT', age: 'NUMERIC')
     expect(test).to eq 1
-    # test = Role.redi_search_schema(name: 'TEXT')
-    # expect(test).to eq 1
+    test = Role.redi_search_schema(name: 'TEXT')
+    expect(test).to eq 1
   end
 
   it "ft_search" do
     User.ft_create
     User.ft_add(User.new(name: 'Bob Smith', age: 100))
     User.ft_add(User.new(name: 'Bobs', age: 50))
+    Role.ft_create
+    Role.ft_add(Role.new(name: 'admin'))
+    # =>
     test = User.ft_search('bob')
-    expect(test).not_to be nil
     expect(test.count).to eq 2
     expect(test[0]['name']).to eq 'Bob Smith'
     expect(test[0]['age']).to eq '100'
     expect(test[1]['name']).to eq 'Bobs'
     expect(test[1]['age']).to eq '50'
+    # =>
+    test = Role.ft_search('admin')
+    expect(test.count).to eq 1
+    expect(test[0]['name']).to eq 'admin'
   end
 
   it "ft_create" do
-    test = User.ft_create
-    expect(test).to eq 0
+    expect(User.ft_create).to eq 0
+    expect(Role.ft_create).to eq 0
   end
 
   it "ft_add_all" do
@@ -43,38 +49,46 @@ RSpec.describe RediSearchRails do
     User.new(name: 'Bob', age: 100)
     User.new(name: 'Bobs', age: 50)
     User.ft_add_all
+    # => TODO: all and each methods not present for current test records
   end
 
   it "ft_add" do
     User.ft_create
+    Role.ft_create
     test = User.ft_add(User.new(name: 'Bob Smith', age: 100))
     expect(test).to eq 'OK'
-    test2 = User.ft_search('bob')
-    expect(test2[0]['age']).to eq '100'
+    test = Role.ft_add(User.new(name: 'admin'))
+    expect(test).to eq 'OK'
+    test = User.ft_search('bob')
+    expect(test[0]['age']).to eq '100'
   end
 
   it "ft_del" do
     User.ft_create
-    test = User.ft_del(User.new)
-    expect(test).to eq 0
+    Role.ft_create
+    expect(User.ft_del(User.new(name: 'bob'))).to eq 0
+    expect(Role.ft_del(User.new(name: 'bob'))).to eq 0
   end
 
   it "ft_optimize" do
     User.ft_create
-    test = User.ft_optimize
-    expect(test).to eq 0
+    expect(User.ft_optimize).to eq 0
+    Role.ft_create
+    expect(Role.ft_optimize).to eq 0
   end
 
   it "ft_drop" do
     User.ft_create
-    test = User.ft_drop
-    expect(test).to eq 'OK'
+    Role.ft_create
+    expect(User.ft_drop).to eq 'OK'
+    expect(Role.ft_drop).to eq 'OK'
   end
 
   it "ft_info" do
     User.ft_create
-    test = User.ft_info
-    expect(test).not_to be nil
+    Role.ft_create
+    expect(User.ft_info).not_to be nil
+    expect(Role.ft_info).not_to be nil
   end
 
 end
